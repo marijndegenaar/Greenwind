7b8c330a7017f07e28c104edf66201576fe35641
marijndegenaar <marijndegenaar@gmail.com>
2017-08-02 19:25:22 +0200

    0.1 get started
    with sass!

---
+++ static/src/components/Homepage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg.en }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: { en: 'Welcome to Greenwind', de: 'Heil' }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+h1, h2
+  font-weight: normal
+a
+  color: #42b983
+</style>
